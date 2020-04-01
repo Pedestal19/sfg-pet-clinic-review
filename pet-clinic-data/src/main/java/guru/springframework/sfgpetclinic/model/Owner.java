eceb8f3db3670c7b4d68db0215887cd11fcd21e6
HosannaGabeOji <gabeoji01@gmail.com>
2020-04-02 00:21:27 +0100

    added visits, updated owner with pet relationship. closes #26

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,9 +1,21 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Author: Hosanna Gabe-Oji.
  * Project: sfg-pet-clinic.
  * Date: 11/11/2019.
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
