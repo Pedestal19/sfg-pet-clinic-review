985f026e7f9700f2be8f5d9709e8f56c1e08824b
HosannaGabeOji <gabeoji01@gmail.com>
2020-04-02 01:21:52 +0100

    added PetTypeService. closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * Author: Hosanna Gabe-Oji.
+ * Project: sfg-pet-clinic.
+ * Date: 4/2/2020.
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
