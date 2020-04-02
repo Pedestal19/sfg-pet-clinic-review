5321fd9a7f8992ce97c072dfe46a34aa21ed1007
HosannaGabeOji <gabeoji01@gmail.com>
2020-04-02 02:42:14 +0100

    added specialties map impl. closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+/**
+ * Author: Hosanna Gabe-Oji.
+ * Project: sfg-pet-clinic.
+ * Date: 4/2/2020.
+ */
+public interface SpecialtiesService extends CrudService<Speciality, Long>{
+}
